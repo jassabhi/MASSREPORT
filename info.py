b3fd00d87cfeb76d05f1e70f451c6f7f1183b0b4
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-06-07 22:14:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,12 +5,12 @@ class Config(object):
     # Pyrogram Client
     API_ID    = int(os.environ.get("API_ID", "24509589"))  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "717cf21d94c4934bcbe1eaa1ad86ae75") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6151534135:AAHoxNXi9STwnkrO8EnJTqqEOks5qO3Z4fA") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7388570343:AAEC9svDM2XZZ4rzyj9L0Tdn-5iclaGB8TM") # ⚠️ Required
     
     # Other Configs
     BOT_START_TIME = time.time()
-    OWNER    = int(os.environ.get("OWNER", "6664582540"))  # ⚠️ Required
-    SUDO = list(map(int, os.environ.get("SUDO", "6664582540").split()))  # ⚠️ Required
+    OWNER    = int(os.environ.get("OWNER", "6352107773"))  # ⚠️ Required
+    SUDO = list(map(int, os.environ.get("SUDO", "6352107773").split()))  # ⚠️ Required
     # Web Response Config
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8080"))
