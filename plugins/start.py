ad1416e9cbe6493fa8cd4f54701e4039b0e51ecd
jassabhi <123294182+jassabhi@users.noreply.github.com>
2024-07-01 13:25:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,7 +12,7 @@ async def handle_start(bot:Client, message:Message):
     Btn = [
         [InlineKeyboardButton(text='⛑️𝖧𝖤𝖫𝖯🚁', callback_data='help'), InlineKeyboardButton(text='🌀𝖡𝖮𝖳 𝖲𝖳𝖠𝖳𝖴𝖲✳️', callback_data='server')],
         [InlineKeyboardButton(text='📰𝖴𝖯𝖣𝖠𝖳𝖤🗞️', url='https://t.me/PBX_PERMOT'), InlineKeyboardButton(text='🤖𝖡𝖮𝖳 𝖨𝖭𝖥𝖮ℹ️', callback_data='about')],
-        [InlineKeyboardButton(text='🧑‍💻𝖣𝖤𝖵⌨️', url='https://t.me/II_BAD_BABY_II')]
+        [InlineKeyboardButton(text='🧑‍💻𝖣𝖤𝖵⌨️', url='https://t.me/ll_BAD_BBY_ll')]
         ]
 
     await message.reply_text(text=Txt.START_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(Btn))
